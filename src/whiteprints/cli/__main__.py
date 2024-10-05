c705edca8a62ad61e56b854ae50fb6e64b33b871
Romain Brault <mail@romainbrault.com>
2024-10-05 20:51:33 +0200

    fix: wrong copyright holders

--- src/whiteprints/cli/__main__.py
+++ src/whiteprints/cli/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# SPDX-FileCopyrightText: © 2024 Romain Brault <mail@romainbrault.com>
+# SPDX-FileCopyrightText: © 2024 The Whiteprints authors and contributors <whiteprints@pm.me>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
