40d4ec01be16eab380193850e79f22ff6248a1c9
Romain Brault <1455095+RomainBrault@users.noreply.github.com>
2024-10-15 13:22:43 +0200

    feat: :sparkles:  lazy loading of the init command (#45)
    <!--
    # Foreword
    
        We are happy to accept contributions from our users 🚀.
    
        For more details on how to contribute see
    
    [CONTRIBUTING.md](https://github.com/whiteprints/whiteprints/blob/main/CONTRIBUTING.md).
    
        We follow (and lint) Pull Requests names according to
    [Angular commit
    format](https://gist.github.com/brianclements/841ea7bffdb01346392c#file-commit-formatting-md)
    
        If this is your first contribution, feel free to add yourself as a
        contributor. To do so comment the pull request with:
        @all-contributors please add @<username> for <contributions>.
    Please refer to the documentation of allcontributors to see the list of
    [contribution
    types](https://allcontributors.org/docs/en/emoji-key#docsNav)
    -->
    
    # Description
    
    Better code logic separation and performances improvement.
    
    # Checklist:
    
      - Quality check
    
    - I agree to follow this project's [Code of
    Conduct](https://github.com/whiteprints/whiteprints/blob/main/CODE_OF_CONDUCT.md)
    - I have read the [Contributor
    Guide](https://github.com/whiteprints/whiteprints/blob/main/CONTRIBUTING.md)
        - I have performed a self-review of my own code
        - I have included relevant tests
        - I have commented my code, particularly in hard-to-understand areas
        - I have made corresponding changes to the documentation
    
      - By making a contribution to this project, I certify that:
    
    - The contributor represents and warrants, on behalf of their employer
    or other principal if they are acting within the scope of their
    employment or otherwise as the agent of a legal entity, that they have
    the right and authority to make their contribution under these terms.
    - The contribution was created in whole or in part by me and I have the
    right to submit it under the license of the file(s) modified or created;
    **or**
    - the contribution is based upon previous work that, to the best of my
    knowledge, is covered under an appropriate open source license and I
    have the right under that license to submit that work with
    modifications, whether created in whole or in part by me, under the
    license of the file(s) modified or created.

---
+++ src/whiteprints/cli/init.py
@@ -0,0 +1,238 @@
+# SPDX-FileCopyrightText: © 2024 The "Whiteprints" contributors <whiteprints@pm.me>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Initialize a project."""
+
+from collections.abc import Iterable
+from pathlib import Path
+from subprocess import CalledProcessError  # nosec
+from typing import Final
+
+from click import ClickException
+from typing_extensions import Unpack
+
+from whiteprints.cli.init_interface import InitKwargs
+from whiteprints.copier_run import Copier
+
+
+__all__: Final = ["CopierCopyError", "init"]
+
+
+WHITEPRINTS_TEMPLATE_CONTEXT_VERSION: Final = "0.2.1"
+"""The whiteprints-template-context version pin."""
+
+
+class CopierCopyError(ClickException):  # pragma: no cover
+    """An error occured while creating the project."""
+
+    def __init__(self) -> None:
+        """Create an exception instance."""
+        super().__init__("Project creation failed.")
+
+
+def _should_add(feature: str, cli_kwargs: InitKwargs) -> bool:
+    """Whether a GitHub feature should be added.
+
+    Args:
+        feature: the feature to add.
+        cli_kwargs: the command-line key-value arguments.
+
+    Returns:
+        True if the feature should be added, False otherwise.
+    """
+    return cli_kwargs.get(feature, False) or cli_kwargs["github_all"]
+
+
+def add_github_functionalities(
+    copier: Copier,
+    *,
+    copier_args: Iterable[str],
+    project_directory: str,
+    **kwargs: Unpack[InitKwargs],
+) -> None:
+    """Update the project to add GitHub functionalities.
+
+    Args:
+        copier: a copier manager.
+        copier_args: additional arguments forwarded to copier.
+        project_directory: directory where the new project will be created.
+        kwargs: the command line flags.
+    """
+    if _should_add("pypi", cli_kwargs=kwargs):  # pragma: no cover
+        copier.copy(
+            [
+                "gh:whiteprints/template-github-publish-pypi.git",
+                project_directory,
+                *copier_args,
+            ],
+            context=[
+                "whiteprints-template-context=="
+                + WHITEPRINTS_TEMPLATE_CONTEXT_VERSION
+            ],
+            trust=True,
+        )
+
+    if _should_add("codecov", cli_kwargs=kwargs):  # pragma: no cover
+        copier.copy(
+            [
+                "gh:whiteprints/template-github-codecov.git",
+                project_directory,
+                *copier_args,
+            ],
+            context=[
+                "whiteprints-template-context=="
+                + WHITEPRINTS_TEMPLATE_CONTEXT_VERSION
+            ],
+            trust=True,
+        )
+
+    if (  # pragma: no cover
+        _should_add("protect_repository", cli_kwargs=kwargs)
+    ):
+        copier.copy(
+            [
+                "gh:whiteprints/template-github-protect-repository.git",
+                project_directory,
+                *copier_args,
+            ],
+            context=[
+                "whiteprints-template-context=="
+                + WHITEPRINTS_TEMPLATE_CONTEXT_VERSION
+            ],
+            trust=True,
+        )
+
+
+def _require_github(**kwargs: Unpack[InitKwargs]) -> bool:
+    """Check if the project requires a GitHub configuration.
+
+    Args:
+        kwargs: the command line flags.
+
+    Returns:
+        True if the project requires a GitHub configuration, False otherwise.
+    """
+    return (
+        kwargs["pypi"]
+        or kwargs["codecov"]
+        or kwargs["readthedocs"]
+        or kwargs["protect_repository"]
+    )
+
+
+def add_github(
+    copier: Copier,
+    *,
+    copier_args: Iterable[str],
+    project_directory: str,
+    **kwargs: Unpack[InitKwargs],
+) -> None:
+    """Update the project to add GitHub functionalities.
+
+    Args:
+        copier: a copier manager.
+        copier_args: additional arguments forwarded to copier.
+        project_directory: directory where the new project will be created.
+        kwargs: the command line flags.
+    """
+    if (  # pragma: no cover
+        kwargs["github"] or kwargs["github_all"] or _require_github(**kwargs)
+    ):
+        copier.copy(
+            [
+                "gh:whiteprints/template-github.git",
+                project_directory,
+                *copier_args,
+            ],
+            context=[
+                "whiteprints-template-context=="
+                + WHITEPRINTS_TEMPLATE_CONTEXT_VERSION
+            ],
+            trust=True,
+        )
+
+    add_github_functionalities(
+        copier,
+        copier_args=copier_args,
+        project_directory=project_directory,
+        **kwargs,
+    )
+
+
+def create_project(
+    copier: Copier,
+    *,
+    copier_args: Iterable[str],
+    project_directory: str,
+    **kwargs: Unpack[InitKwargs],
+) -> None:
+    """Initialize a python project.
+
+    Args:
+        copier: a copier manager.
+        copier_args: additional arguments forwarded to copier.
+        project_directory: directory where the new project will be created.
+        kwargs: the command line flags.
+    """
+    copier.copy(
+        [
+            "gh:whiteprints/template-python.git",
+            project_directory,
+            *copier_args,
+        ],
+        context=[
+            "whiteprints-template-context=="
+            + WHITEPRINTS_TEMPLATE_CONTEXT_VERSION
+        ],
+        trust=True,
+    )
+    if kwargs["command_line"]:  # pragma: no cover
+        copier.copy(
+            [
+                "gh:whiteprints/template-rich-click.git",
+                project_directory,
+                *copier_args,
+            ],
+            context=[
+                "whiteprints-template-context=="
+                + WHITEPRINTS_TEMPLATE_CONTEXT_VERSION
+            ],
+            trust=True,
+        )
+
+    add_github(
+        copier,
+        copier_args=copier_args,
+        project_directory=project_directory,
+        **kwargs,
+    )
+
+
+def init(
+    project_directory: Path,
+    copier_args: Iterable[str],
+    **kwargs: Unpack[InitKwargs],
+) -> None:
+    """Initialize a python project.
+
+    Args:
+        project_directory: directory the new project will be created.
+        copier_args: arguments forwarded to copier.
+        kwargs: the command line flags.
+
+    Raises:
+        CopierCopyError: An error happened while creating the project.
+    """
+    copier = Copier()
+    project_directory_str = str(project_directory)
+
+    try:
+        create_project(
+            copier,
+            copier_args=copier_args,
+            project_directory=project_directory_str,
+            **kwargs,
+        )
+    except CalledProcessError as process_error:  # pragma: no cover
+        raise CopierCopyError from process_error
