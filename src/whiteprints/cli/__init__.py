778d3174d693fb5d5177601b19447e2bbe0d3a58
Romain Brault <1455095+RomainBrault@users.noreply.github.com>
2025-01-09 16:47:37 +0100

    fix: :bug: debug justfile (#159)
    # Description
    
    * Fixes #xxxx  <!-- reference an issue here -->
    
    ## 📝Changes
    
    <!-- Please replace and include a quick summary of the change and which
    issue is fixed here start -->
    
    Quick summary of the changes.
    
    <!-- Please replace and include a quick summary of the change and which
    issue is fixed here end -->
    
    ---
    
    ## 🚨 Information
    
    <details>
    <summary><b>🙏 New contributors, please read!</b></summary>
      <p>We are happy to accept contributions from our users 🚀.</p>
      <ul>
    <li>Before submitting the pull request, please ensure the PR follows the
    <a
    href="https://github.com/whiteprints/whiteprints/blob/main/CONTRIBUTING.md">Contributing
    Guide</a>.</li>
    <li>We follow (and lint) Pull Requests names according to <a
    href="https://gist.github.com/brianclements/841ea7bffdb01346392c#file-commit-formatting-md">Angular
    commit format</a>.</li>
    <li>You can choose an emoji for your pull request title. Have a look to
    <a href="https://gitmoji.dev/">gitmoji</a> to guide you in the
    choice.</li>
    <li>If this is your first contribution, feel free to add yourself as a
    contributor. To do so comment the pull request with:
    <code>@all-contributors please add @<username> for
    <contributions></code>. Please refer to the documentation of
    allcontributors to see the list of <a
    href="https://allcontributors.org/docs/en/emoji-key#docsNav">contribution
    types</a>.</li>
    <li>We demand that you follow our <a
    href="https://github.com/whiteprints/whiteprints/blob/main/CODE_OF_CONDUCT.md">Code
    of Conduct</a>.</li>
      </ul>
    </details>
    
    <details>
    <summary><b>🛂 Checklist</b></summary>
        <p>Please check the following:</p>
        <ul>
    <li>I agree to follow this project's <a
    href="https://github.com/whiteprints/whiteprints/blob/main/CODE_OF_CONDUCT.md">Code
    of Conduct</a></li>
    <li>I have read the <a
    href="https://github.com/whiteprints/whiteprints/blob/main/CONTRIBUTING.md">Contributor
    Guide</a></li>
            <li>I have performed a self-review of my own code</li>
            <li>I have included relevant tests</li>
    <li>I have commented my code, particularly in hard-to-understand
    areas</li>
            <li>I have made corresponding changes to the documentation</li>
        </ul>
    </details>
    
    ## ⚖️ Contributor License Agreement
    
    By making a contribution to this project, I (the contributor) hereby
    certify that
    
    - The contributor represents and warrants, on behalf of their employer
    or other principal **if** they are acting within the scope of their
    employment or otherwise as the agent of a legal entity, that they have
    the right and authority to make their contribution under these terms.
    - The contribution was created in whole or in part by me and I have the
    right to submit it under the license of the file(s) modified or created;
    **or**
    - the contribution is based upon previous work that, to the best of my
    knowledge, is covered under an appropriate open source license and I
    have the right under that license to submit that work with
    modifications, whether created in whole or in part by me, under the
    license of the file(s) modified or created.
    
    
    <!-- readthedocs-preview whiteprints start -->
    ----
    📚 Documentation preview 📚:
    https://whiteprints--159.org.readthedocs.build/en/159/
    
    <!-- readthedocs-preview whiteprints end -->

--- src/whiteprints/cli/__init__.py
+++ src/whiteprints/cli/__init__.py
@@ -14,5 +14,7 @@ __all__: Final = ["APP_NAME", "__app_name__"]
 __app_name__: Final = "whiteprints"
 """The name of the application."""
 
-APP_NAME = exception.check_app_name(__app_name__).replace("-", "_").upper()
+APP_NAME: Final = (
+    exception.check_app_name(__app_name__).replace("-", "_").upper()
+)
 """The name of the application in capital letters."""
