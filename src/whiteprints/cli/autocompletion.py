2328ede4509dc13a32c96d62fe41fcf273bc3270
Romain Brault <mail@romainbrault.com>
2025-03-17 15:34:14 +0100

    feat: update versions

--- src/whiteprints/cli/autocompletion.py
+++ src/whiteprints/cli/autocompletion.py
@@ -6,26 +6,6 @@
 
 import importlib
 from argparse import ArgumentParser
-from functools import cache
-
-
-@cache
-def argcomplete_installed() -> bool:
-    """Test if the `argcomplete` module is installed.
-
-    Returns:
-        True if `argcomplete` is installed, False otherwise.
-    """
-    try:
-        importlib.import_module("argcomplete")
-    except ImportError:
-        logger = importlib.import_module("logging").getLogger("entrypoint")
-        logger.info(
-            "No autocompletion available (argcomplete is not installed)"
-        )
-        return False
-
-    return True
 
 
 def argcomplete(parser: ArgumentParser) -> None:
@@ -33,6 +13,4 @@ def argcomplete(parser: ArgumentParser) -> None:
 
     Does nothing if argcomplete is not installed.
     """
-    if argcomplete_installed():
-        argcomplete = importlib.import_module("argcomplete")
-        argcomplete.autocomplete(parser)
+    importlib.import_module("argcomplete").autocomplete(parser)
