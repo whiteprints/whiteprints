f738d1eecae6117d46e76102e37f14b1312d61b4
Romain Brault <mail@romainbrault.com>
2025-03-20 20:03:08 +0100

    feat: update versions

---
+++ src/whiteprints/exception.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: © 2024 The "Whiteprints" contributors <whiteprints@pm.me>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Project specific exception."""
+
+
+class WhiteprintsError(Exception):
+    """A base exception for the project."""
---
+++ src/whiteprints/libuv/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: © 2024 The "Whiteprints" contributors <whiteprints@pm.me>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""UV and UV managed applications."""
