173fab34375cb0b87d7eb1fc1c8ee308367e00de
Romain Brault <mail@romainbrault.com>
2025-03-21 14:04:50 +0100

    feat: :sparkles: better workflow

--- tests/test_whiteprints/test_cli/test_entrypoint.py
+++ tests/test_whiteprints/test_cli/test_entrypoint.py
@@ -13,32 +13,32 @@ from whiteprints.cli.entrypoint import entrypoint
 from whiteprints.package_metadata import __version__
 
 
-def test_help(capsys: pytest.CaptureFixture[AnyStr]) -> None:
+def test_help(capfd: pytest.CaptureFixture[AnyStr]) -> None:
     """Test wether a help flag exists and works."""
     with pytest.raises(SystemExit):
         entrypoint(["--help"])
 
-    captured = capsys.readouterr()
+    captured = capfd.readouterr()
     assert captured.out, "Could not print application help message"
 
 
-def test_version(capsys: pytest.CaptureFixture[AnyStr]) -> None:
+def test_version(capfd: pytest.CaptureFixture[AnyStr]) -> None:
     """Test wether a version flag exists and works."""
     with pytest.raises(SystemExit):
         entrypoint(["--version"])
 
-    captured = capsys.readouterr()
+    captured = capfd.readouterr()
     assert captured.out.rstrip() == __version__, (
         "Printed version does not match library version"
     )
 
 
-def test_debug_info(capsys: pytest.CaptureFixture[AnyStr]) -> None:
+def test_debug_info(capfd: pytest.CaptureFixture[AnyStr]) -> None:
     """Test wether a debug-info flag exists and works."""
     with pytest.raises(SystemExit):
         entrypoint(["--debug-info"])
 
-    captured = capsys.readouterr()
+    captured = capfd.readouterr()
     assert json.loads(captured.out), (
         "Could not print application debug informations"
     )
