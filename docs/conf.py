26f50b27311fd7916a1880dbfff2ceb93b825d07
Romain Raymond BRAULT <account@romainbrault.com>
2025-05-10 08:49:06 +0200

    feat: :sparkles: better workflow

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ language = "en"
 myst_heading_anchors = 3
 extensions = [
     "myst_parser",
-    "sphinx_click",
+    "sphinx_argparse",
     "sphinx_copybutton",
     "sphinx.ext.autosummary",
     "sphinx.ext.viewcode",
